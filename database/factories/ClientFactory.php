5cd4326052c06dec70a6d2402a0a508e10c249a0
NOVITZ Jean-Philippe <novitz@gmail.com>
2019-11-05 14:16:43 +0100

    seeds

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+use App\Client;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -16,7 +16,7 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Client::class, function (Faker $faker) {
     return [
         'company' => $faker->company,
         'firstname' => $faker->firstName,
@@ -25,12 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'nr' => $faker->numberBetween(1, 100),
         'phone' => $faker->phoneNumber,
         'email' => $faker->email,
-        'name' => $faker->userName,
-        'tva' => $faker->vat,
-        'city_id' => $faker->numberBetween(1, 1000),
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'vat' => $faker->vat,
+        'city_id' => $faker->numberBetween(1, 1000)
     ];
 });
