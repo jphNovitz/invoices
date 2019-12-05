b0caf97d8ab1e5c8ef72234f27a1b15871d60301
NOVITZ Jean-Philippe <novitz@gmail.com>
2019-12-05 21:26:19 +0100

    refactor (show invoice) more queries in controller view receive all necessary datas total is calculated in controller just shown in view

---
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Invoice;
+
+use App\client;
+use App\Http\Controllers\Client\ClientController;
+use App\invoice;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InvoiceController extends Controller
+{
+    public function index(client $client){
+        $invoices = invoice::where('client_id', $client->id)->orderBy('created_at', 'DESC')->get();
+        return view('Invoice.list', [
+            'client'=>$client,
+            'invoices' => $invoices
+        ]);
+    }
+
+    public function card(client $client, invoice $invoice){
+
+        return view('Invoice.card', ['client'=>$client, 'invoice'=> $invoice]);
+    }
+    /* idem au dessus choisir un ou voir differences */
+    public function show($id){
+
+        $invoice = invoice::with('Client')->where('id', $id)->first();
+        $total = 0;
+        foreach ($invoice->items as $item):
+            $total  += ((int)$item->price*(float)$item->vat['rate'] + (int)$item->price ) *(int)$item->qty - (int)$item->discount;
+        endforeach;
+        return view('Invoice.show', ['invoice'=> $invoice, 'total' => $total]);
+    }
+
+    public function delete(client $client, invoice $invoice){
+        dump($client);
+        dump($invoice);
+        die();
+    }
+
+
+
+    public function create(){
+        return view('Invoice.create');
+    }
+}
--- app/Item.php
+++ app/Item.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class item extends Model
 {
     public function vat(){
-        return $this->hasOne('App\Vat', 'id');
+        return $this->hasOne('App\Vat', 'id', 'vat_id');
     }
 }
