e8d2554744a4216d4b3d47c6ea8f8b65b6c03e78
Novitz Jean-Philippe <novitz@gmail.com>
2022-01-16 02:01:03 +0100

    Innvoice creation completed - create invoice - add items - check if user > client relation exist if not create - redirect

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -22,7 +22,7 @@ class InvoiceController extends Controller
     public function index()
     {
         $invoices = invoice::where('user_id', \Auth::user()->id)->orderBy('created_at', 'DESC')->get();
-        dd($invoices);
+        
         return view('Invoice.list', [
             'invoices' => $invoices
         ]);
@@ -84,36 +84,28 @@ class InvoiceController extends Controller
 
     public function store(Request $request)
     {
-//        dd($request->client_id);
-//        dd($request->all());
+        // get the user
         $user = auth()->user();
-        $client = Client::where('id', '=', $request->client_id)->first();
-//        dd($client);
-        $invoice = new invoice();
-       /* $invoice->client = $client;
-        $invoice->user = $user;*/
+        $clients = Client::all();
+        $client = $clients->find($request->client_id);
 
-        $user = auth()->user();
-//        $client->users()->save($user);
-        $user->clients()->save($client);
+          if(null === $user->clients()->find($client->id) ) $user->clients()->attach($client);
 
         // The invoice
         $invoice_datas['reference'] = 'toto';
-        $invoice_datas['client_id'] =  $client->id;
-        $invoice_datas['user_id'] =  $user->id;
+        $invoice_datas['client_id'] = $client->id;
+        $invoice_datas['user_id'] = $user->id;
 
+        $invoice = new invoice();
         $last_added = $invoice->create($invoice_datas);
-        $last_id= $last_added->id;
+        $last_id = $last_added->id;
 
         // The items
         foreach ($request->all()['items'] as $item) {
-//            $item['invoice_id'] = $last_id;
             $items_to_add[] = $item;
         }
-//        $invoice->items()->createMany($items_to_add);
-//$last_added->items()->sync($request->all()['items']);
         $last_added->items()->createMany($items_to_add);
-        die;
-        return redirect(route('clients_home'));
+
+        return redirect(route('all_users_invoices_list'))->with('success', 'invoice created');
     }
 }
