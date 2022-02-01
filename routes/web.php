83ac87ce81af8a12d372c5e8dbae6f0115f858d2
Novitz Jean-Philippe <novitz@gmail.com>
2022-02-02 00:02:20 +0100

    invoices  routes -> create prefix route

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::post('/user/store', 'User\UserController@store')->name('user_store');
 Route::get('/clients', 'Client\ClientController@index')->name('clients_home');
 Route::get('/client/{client}', 'Client\ClientController@show')->name('client_card');
 Route::get('/client/{client}/update', 'Client\ClientController@update')->name('client_update');
-Route::get('/client/{client}/delete', function (App\Client $client){
+Route::get('/client/{client}/delete', function (App\Client $client) {
     return view('Client.client-delete', ['client' => $client]);
 })->name('client_delete');
 Route::delete('/client/{client}/delete', 'Client\ClientController@delete')->name('client_remove');
@@ -36,16 +36,16 @@ Route::get('/clients/new', 'Client\ClientController@create')->name('clients_crea
 Route::post(null, 'Client\ClientController@store')->name('client_store');
 Route::put(null, 'Client\ClientController@save')->name('client_save');
 
-Route::get('/api/client/{client}', 'Client\ApiClientController@show')->name('api_client_infos');
-
-/* Invoices */;
+Route::get('/api/client/{client}', 'Client\ApiClientController@show')->name('api_client_infos');/* Invoices */;
 
 /* Users's invoices */
-Route::get('/invoices', 'Invoice\InvoiceController@index')->name('invoices_list');
-Route::get('/invoice/show/{id}', 'Invoice\InvoiceController@show')->name('invoice_show')->middleware('owned_invoice');
-Route::get('/invoice/add/{id?}', 'Invoice\InvoiceController@create')->name('invoice_create');
-Route::post('/invoice/store', 'Invoice\InvoiceController@store')->name('invoice_store');
-Route::get('/invoice/edit/{id}', 'Invoice\InvoiceController@edit')->name('invoice_edit')->middleware('owned_invoice');
-Route::put('/invoices/update', 'Invoice\InvoiceController@update')->name('invoice_update');
-Route::get('/invoice/delete/{id}', 'Invoice\InvoiceController@delete')->name('invoice_delete')->middleware('owned_invoice');
-Route::post('/invoice/remove', 'Invoice\InvoiceController@remove')->name('invoice_remove');
+Route::prefix('invoice')->group(function () {
+    Route::get('/', 'Invoice\InvoiceController@index')->name('invoices_list');
+    Route::get('/show/{id}', 'Invoice\InvoiceController@show')->name('invoice_show')->middleware('owned_invoice');
+    Route::get('/add/{id?}', 'Invoice\InvoiceController@create')->name('invoice_create');
+    Route::post('/store', 'Invoice\InvoiceController@store')->name('invoice_store');
+    Route::get('/edit/{id}', 'Invoice\InvoiceController@edit')->name('invoice_edit')->middleware('owned_invoice');
+    Route::put('/update', 'Invoice\InvoiceController@update')->name('invoice_update');
+    Route::get('/delete/{id}', 'Invoice\InvoiceController@delete')->name('invoice_delete')->middleware('owned_invoice');
+    Route::post('/remove', 'Invoice\InvoiceController@remove')->name('invoice_remove');
+});
