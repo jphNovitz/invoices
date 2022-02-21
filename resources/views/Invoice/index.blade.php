7f888549a47fe3ea2f91e5f1bdee9536e9b1c45d
Novitz Jean-Philippe <novitz@gmail.com>
2022-02-22 00:26:40 +0100

    invoices list

--- resources/views/Invoice/index.blade.php
+++ resources/views/Invoice/index.blade.php
@@ -8,105 +8,82 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(Session::has('message'))
-        <p class="alert alert-info">{{ Session::get('message') }}</p>
-    @endif
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Factures</h2>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <a href="{{route('invoice_create')}}" class="btn btn-primary">
-                                    {{__('btn.New_invoice')}}
-                                </a>
+    <section class="card w-full  mb-10 border p-12px border-slate-200">
+        <div class="card-header bg-slate-500 text-slate-50">
+            Liste de vos factures
+        </div>
+
+        <div class="card-body w-full">
+            <div class="list py-4 ">
+        <span class="button secondary p-3 mx-1 w-40">
+            <a href="{{route('invoice_create')}}">
+                <i class="fas fa-plus"></i>
+            </a>
+        </span>
+                @foreach($invoices as $invoice )
+                    <article
+                            class="odd:bg-white even:bg-slate-100 overflow-hidden w-full my-6  px-3 flex flex-col justify-between">
+                        <div class="w-full flex flex-row justify-between">
+                            <div class="font-black flex flex-col">
+                                {{$invoice->client->company}} ({{$invoice->total }} eur)
+                                <span>Ref: {{$invoice->reference}}</span></div>
+                            <div class="actions flex flex-row justify-end items-center my-auto">
+                               <span class="button-text self-start action-toggle transition-transform ease-in duration-400">
+                                   <span class="icon primary ">
+                                        <i class="fas fa-chevron-down"></i>
+                                   </span>
+{{--                                   <span class="label ">&nbsp; </span>--}}
+                               </span>
+                                <div class="button-text">
+                                    <a href="{{route('invoice_show', ["id"=>$invoice->id])}}">
+                                        <span class="flex m-auto w-7 h-7 items-center justify-center rounded-full info ">
+                                            <i class="fas fa-search-plus"></i>
+                                        </span>
+                                        <span class="hidden flex md:flex flex-col text-sm">{{__('btn.Details')}}</span>
+                                    </a>
+                                </div>
+                                <span class="button-text p-3">
+                                    <a href="{{route('invoice_show', ["id"=>$invoice->id])}}">
+                                        <span class="icon success">
+                                             <i class="fas fa-edit"></i>
+                                        </span>
+                                        <span class="label">
+                                            {{__('btn.Update')}}
+                                        </span>
+                                    </a>
+                                </span>
+                                <span class="button-text">
+                                    <a href="{{route('invoice_show', ["id"=>$invoice->id])}}">
+                                        <span class="icon danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </span>
+                                        <span class="label">
+                                            {{__('btn.Remove')}}
+                                        </span>
+                                    </a>
+                                </span>
+
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-12">
-
-                                <h3>Liste des factures</h3>
-                                @foreach($invoices as $invoice)
-                                    <div class="row">
-                                        <div class="col-md-12 grid">
-                                            <div class="invoice-list-group">
-                                                <div class="row trigger-part">
-                                                    <div class="col sm-6  d-none d-md-block d-xl-none">a</div>
-                                                    <div class="col-12 col-md-2">{{$invoice->created_at->format('d/m/Y')}}</div>
-                                                    <div class="col-12 col-md-4">
-                                                        Réf: {{$invoice->reference}}</div>
-                                                    <div class="col-12 col-md-4">{{$invoice->client->company}}</div>
-                                                    <div class="col-12 col-md-2">
-                                                        @if($invoice->total) {{$invoice->total}}
-                                                        @else N/A
-                                                        @endif
-                                                    </div>
-                                                </div>
-                                                <div class="hidden-part">
-                                                    <div class="row">
-                                                        <div class="col-12 col-md-4">
-                                                            TVA N° {{$invoice->client->vat}}
-                                                        </div>
-                                                        <div class="col-12 col-md-4">
-                                                            {{$invoice->client->lastname}} {{$invoice->client->firstname}}
-                                                        </div>
-                                                        <div class="col-12 col-md-2">
-                                                            <strong>HTVA: </strong>
-                                                            @if($invoice->exvat)
-                                                                {{$invoice->exvat}}
-                                                            @else N/A
-                                                            @endif
-                                                        </div>
-                                                        <div class="col-12 col-md-2">
-                                                            <strong>TVA: </strong>
-                                                            @if($invoice->vat)
-                                                                {{$invoice->vat}}
-                                                            @else N/A
-                                                            @endif
-                                                        </div>
-                                                    </div>
-                                                    <div class="row invoice-actions">
-
-                                                        <a href="{{route('invoice_show', ['id'=>$invoice->id])}}"
-                                                           class="btn btn-sm btn-primary">
-                                                            <i class="fas fa-info"></i>
-                                                            {{__('btn.Details')}}
-                                                        </a>
-
-                                                        <a href="{{route('invoice_edit', ['id'=>$invoice->id])}}"
-                                                           class="btn btn-sm btn-primary">
-                                                            <i class="fas fa-info"></i>
-                                                            {{__('btn.Update')}}
-                                                        </a>
-
-                                                        <a href="{{route('invoice_delete', ['id'=>$invoice->id])}}"
-                                                           class="btn btn-sm btn-danger">
-                                                            <i class="fas fa-trash"></i>
-                                                            {{__('btn.Remove')}}
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-
-                            </div>
-                            <div class="col-md-12  pt-5">
-                            {{$invoices->links()}}
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+                        <ul class="infos flex flex-col h-0 transition-height ease-in duration-600">
+                            <li>{{__('auth.Lastname')}}: <strong>{{$invoice->client->lastname}}</strong></li>
+                            <li>{{__('auth.Firstname')}}: <strong>{{$invoice->client->firstname}}</strong></li>
+                            <li>{{__('auth.Vat')}}: <strong>T{{$invoice->client->vat}}</strong></li>
+                            <li>{{__('auth.Email')}}: <strong>{{$invoice->client->email}}</strong></li>
+                            <li>{{__('auth.Street')}}:
+                                <strong>{{$invoice->client->street}} {{$invoice->client->nr}}</strong></li>
+                        </ul>
+                    </article>
+                    {{--                    {{dd($client)}}--}}
+                @endforeach
             </div>
         </div>
-    </div>
+        <div class="card-footer bg-slate-200 text-slate-700 flex flex-row justify-center">
+            {{ $invoices->links('pagination::tailwind') }}
+
+        </div>
+    </section>
 
 @endsection
 
