da2338bbb53e3cc17c9ad8a3d5ec28e19a810880
Novitz Jean-Philippe <novitz@gmail.com>
2022-02-28 16:41:56 +0100

    ...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Client;
+use App\Invoice;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,19 +25,29 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $clients_no_invoices =  $clients = \Auth::user()
+        $clients_no_invoices = $clients = \Auth::user()
             ->clients()
             ->whereDoesntHave('invoices')
             ->orderBy('created_at', 'DESC')
             ->take(5)
             ->get();
         $clients_many_invoices = Client::withCount('invoices')
-            ->orderBy(\DB::raw('invoices_count'),'DESC')
+            ->orderBy(\DB::raw('invoices_count'), 'DESC')
             ->orderBy('created_at', 'DESC')
             ->take(5)->get();
 
+        $invoices = Invoice::with('client')
+            ->where('user_id', \Auth::user()->id)
+            ->orderBy('created_at', 'DESC')
+            ->take(10)
+            ->get();
+
+
         $promote_clients = collect($clients_no_invoices)->merge($clients_many_invoices);
 
-        return view('home', ['promote_clients' => $promote_clients]);
+        return view('home', [
+            'promote_clients' => $promote_clients,
+            'invoices' => $invoices
+        ]);
     }
 }
