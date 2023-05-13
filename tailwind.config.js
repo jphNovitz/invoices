05ab1251e2442e1908b6ca7403df634cc25520c7
Novitz Jean-Philippe <novitz@gmail.com>
2023-05-13 22:07:38 +0200

    refactor bg image config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
               'body': ["Montserrat", "sans-serif"],
             },
             backgroundImage: {
+                // 'hero': "url('/img/screenshot 2023-01.webp')",
+                'hero-mobile': "url('/img/screenshot-test.webp')",
+                'hero-md': "url('/img/screenshot-test.webp')",
                 'auth': "url('/img/invoice-bg.webp')",
             },
             transitionProperty: {
