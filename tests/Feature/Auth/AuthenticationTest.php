fd5e1e28c621cb96778c2cab92a50d6df4849957
Novitz Jean-Philippe <novitz@gmail.com>
2023-05-19 00:22:31 +0200

    new TEST  AuthenticationTest.php test login phase

---
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Database\Seeders\TestSeeder;
+use Illuminate\Foundation\Testing\TestCase ;
+
+
+test('login screen can be rendered', function () {
+    $this->withoutExceptionHandling();
+
+    $response = $this->get('/login');
+
+    $response->assertStatus(200);
+});
+
+test('users can authenticate using the login screen', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+
+    $response = $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('home'));
+});
+
+test('users can not authenticate with invalid password', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+
+    $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+    ]);
+
+    $this->assertGuest();
+});
