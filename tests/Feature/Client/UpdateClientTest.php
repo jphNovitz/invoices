7633cab8c5260fd937cca30de7c6ebb2dab79f0b
Novitz Jean-Philippe <novitz@gmail.com>
2023-07-01 00:25:26 +0200

    new : tests actions - Client update action gets error if fied is missing when submit form - Client is updated if all field is given in submit form

--- tests/Feature/Client/UpdateClientTest.php
+++ tests/Feature/Client/UpdateClientTest.php
@@ -24,7 +24,6 @@ test('client update screen  gets error if  connected but no client id given', fu
 
     $this->seed(TestSeeder::class);
     $user = User::all()->random();
-    $client = $user->clients->first();
 
     $response = $this->actingAs($user)
         ->get(route('client_update'));
@@ -33,94 +32,72 @@ test('client update screen  gets error if  connected but no client id given', fu
     $response->assertSessionHasErrors();
 
 });
-//
-//
-//test('create client screen is accessible if connected', function () {
-//    $this->seed(TestSeeder::class);
-//    $user = User::all()->random();
-//
-//    $response = $this->actingAs($user)
-//        ->get(route('client_create'));
-//
-//    $response->assertStatus(200);
-//
-//});
-//
-//test('create client screen contains city', function () {
-//    $this->seed(TestSeeder::class);
-//    $user = User::all()->random();
-//
-//    $city = City::first();
-//
-//    $response = $this->actingAs($user)
-//        ->get(route('client_create'));
-//
-//    $response->assertStatus(200);
-//    $response->assertSee($city->name);
-//
-//
-//});
-//
-//test('store client action gets error if field is missing when submit form', function () {
-//    $this->seed(TestSeeder::class);
-//    $user = User::all()->random();
-//
-//    $response = $this->actingAs($user)
-//        ->post(route('client_store', [
-//            "firstname" => "Lorem name",
-//            "lastname" => "Last Ipsum",
-//            "company" => "Fake Company",
-//            "vat" => "",
-//            "street" => "Street Lorem",
-//            "nr" => "1",
-//            "phone" => "55534343",
-//            "email" => "fake@email.com",
-//            "city_id" => "1"
-//        ]));
-//
-//    $response->assertStatus(302);
-//});
-//
-//test('store client action redirect to clients list after create', function () {
-//    $this->seed(TestSeeder::class);
-//    $user = User::all()->random();
-//
-//    $response = $this->actingAs($user)
-//        ->post(route('client_store', [
-//            "firstname" => "Lorem name",
-//            "lastname" => "Last Ipsum",
-//            "company" => "Fake Company",
-//            "vat" => "123456789",
-//            "street" => "Street Lorem",
-//            "nr" => "1",
-//            "phone" => "55534343",
-//            "email" => "fake@email.com",
-//            "city_id" => "1"
-//        ]));
-//
-//    $response->assertRedirect(route('clients_list'));
-//
-//});
-//
-//test('new client exists in database after create', function () {
-//    $this->seed(TestSeeder::class);
-//    $user = User::all()->random();
-//
-//    $response = $this->actingAs($user)
-//        ->post(route('client_store', [
-//            "firstname" => "Lorem name",
-//            "lastname" => "Last Ipsum",
-//            "company" => "Fake Company",
-//            "vat" => "123456789",
-//            "street" => "Street Lorem",
-//            "nr" => "1",
-//            "phone" => "55534343",
-//            "email" => "fake@email.com",
-//            "city_id" => "1"
-//        ]));
-//
-//    $client = Client::where("vat", "=", "123456789")->first();
-//
-//    $this->assertNotNull($client);
-//    $this->assertEquals($client->firstname, "Lorem name");
-//});
+
+test('client update screen  is accessible if logged in and client id given', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+    $client = $user->clients->first();
+
+    $response = $this->actingAs($user)
+        ->get(route('client_update', [
+            'client' => $client->id
+        ]));
+
+    $response->assertStatus(200);
+});
+
+
+test('Client update action gets error if fied is missing when submit form', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+    $client = $user->clients->first();
+
+    $client->firstname = "";
+
+    $response = $this->actingAs($user)
+        ->put(route('client_save', [
+            'client' => $client->id,
+            "firstname" => "",
+            "lastname" => "Last Ipsum",
+            "company" => "Fake Company",
+            "vat" => "123456789",
+            "street" => "Street Lorem",
+            "nr" => "1",
+            "phone" => "55534343",
+            "email" => "fake@email.com",
+            "city_id" => "1"
+        ]));
+
+    $response->assertStatus(302);
+    $response->assertSessionHasErrors();
+
+});
+
+test('Client is updated if all field is given in submit form', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+    $client = $user->clients->first();
+
+    $client->firstname = "";
+
+    $response = $this->actingAs($user)
+        ->put(route('client_save', [
+            'id' => $client->id,
+            "firstname" => "My new name",
+            "lastname" => "Last Ipsum",
+            "company" => "Fake Company",
+            "vat" => "123456789",
+            "street" => "Street Lorem",
+            "nr" => "1",
+            "phone" => "55534343",
+            "email" => "fake@email.com",
+            "city_id" => "1"
+        ]));
+
+    $response->assertRedirect(route('clients_list'));
+
+    $response = $this->actingAs($user)
+        ->get(route('clients_list'));
+        $response->assertSee("My new name");
+
+});
