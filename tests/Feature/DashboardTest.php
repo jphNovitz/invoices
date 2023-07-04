3c8f74b0323eb02cd41966d06d682c9c6ec3d174
Novitz Jean-Philippe <novitz@gmail.com>
2023-07-04 21:51:00 +0200

    new : test Dashboard Test

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\City;
+use App\Models\Client;
+use App\Models\User;
+use Database\Seeders\TestSeeder;
+use Illuminate\Support\Facades\Session;
+
+test('dashboard screen is not accessbile if not authenticated', function () {
+    $response = $this->get(route('home'));
+
+    $response->assertStatus(302);
+    $response->assertRedirect(route('login'));
+
+});
+
+test('dashboard screen is if authenticated', function () {
+    $this->seed(TestSeeder::class);
+    $user = User::all()->random();
+
+    $response = $this->actingAs($user)->get(route('home'));
+
+    $response->assertStatus(200);
+
+});
